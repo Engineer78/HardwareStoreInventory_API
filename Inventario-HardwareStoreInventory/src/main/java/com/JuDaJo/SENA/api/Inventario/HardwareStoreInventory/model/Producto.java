9cd17228f31641381d9387cc62579065eec0f272
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-09 18:15:12 -0500

    Se crea el campo cantidad para la entidad producto

--- Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/model/Producto.java
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/model/Producto.java
@@ -37,5 +37,11 @@ public class Producto {
     @NotBlank(message = "El nombre del producto no puede estar en blanco")
     private String nombreProducto;
 
+    /**
+     * Cantidad disponible del producto.
+     */
+    @Column(name = "cantidad")
+    @Min(value = 0, message = "La cantidad no puede ser negativa")
+    private int cantidad;
 
 }
