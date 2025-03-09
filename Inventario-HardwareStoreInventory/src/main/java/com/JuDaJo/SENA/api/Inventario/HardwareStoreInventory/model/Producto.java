5d27e9950f6a58e190b25d1d7788dc060cb494ed
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-09 18:08:47 -0500

    Se crea la clase Producto con sus respectivas importaciones

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/model/Producto.java
@@ -0,0 +1,18 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Representa un producto dentro del inventario.
+ * Contiene información del producto, como código, nombre, cantidad, valor unitario y su relación con la categoría y proveedores.
+ */
+@Entity
+public class Producto {
+}
