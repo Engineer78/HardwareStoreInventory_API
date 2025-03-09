dea9025a7d2316c4ce93474a89f20d460022fafe
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-09 17:14:44 -0500

    Se crean los archivos base del proyecto API Rest usando Spring Boot, JDK17 y Maven

---
+++ Inventario-HardwareStoreInventory/src/test/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/InventarioHardwareStoreInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioHardwareStoreInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
