41add95ebb2df78aabfbad3d4401bf96b28e3e2b
Juan Gallego <homertrixmemo@yahoo.com>
2025-03-09 18:13:24 -0500

    Se crea el nombre del proveedor

--- Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/model/Proveedor.java
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/model/Proveedor.java
@@ -19,4 +19,12 @@ public class Proveedor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idProveedor;
+
+    /**
+     * Nombre del proveedor.
+     */
+    @NotBlank(message = "El nombre del proveedor es obligatorio")
+    @Size(max = 100, message = "El nombre del proveedor no puede exceder los 100 caracteres")
+    @Column(name = "nombre_proveedor")
+    private String nombreProveedor;
 }
