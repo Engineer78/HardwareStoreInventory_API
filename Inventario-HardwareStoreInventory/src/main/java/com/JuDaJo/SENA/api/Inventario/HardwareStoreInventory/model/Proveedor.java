29b218ed4d651169b6e7fc917ef9b4493e40d68f
Juan Gallego <homertrixmemo@yahoo.com>
2025-03-09 18:08:50 -0500

    Se crea la clase Proveedor con sus respectivas importaciones

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/model/Proveedor.java
@@ -0,0 +1,15 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Entidad que representa un proveedor de productos.
+ */
+@Entity
+public class Proveedor {
+}
