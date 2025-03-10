6843493d372c7f7364b94910f473ea9a370e0376
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-09 21:33:15 -0500

    Se crea la clase ValidationErrorDTO para encapsular los mensajes de error de las validaciones para poder utilizar en la capa de presentación, y se agregan las importaciones correspondientes

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ValidationErrorDTO.java
@@ -0,0 +1,16 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.dto;
+
+import org.springframework.validation.ObjectError;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Esta clase representa un DTO (Data Transfer Object) para encapsular
+ * los mensajes de error de validación.
+ * Se ha diseñado esta clase para transformar errores de validación en una lista
+ * de mensajes simples que se pueden utilizar fácilmente en la capa de presentación.
+ */
+
+public class ValidationErrorDTO {
+}
