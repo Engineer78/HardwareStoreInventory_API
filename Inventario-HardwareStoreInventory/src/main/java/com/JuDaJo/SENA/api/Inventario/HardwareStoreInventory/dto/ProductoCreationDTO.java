bb4b0b6eb62b56f88fd9c657b882dc425f96111d
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-09 20:19:45 -0500

    Se crea la clase ProductoCreationDTO para recibir los datos necesarios para crear productos nuevos

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ProductoCreationDTO.java
@@ -0,0 +1,13 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * DTO para la creación de un producto.
+ * Este DTO se utiliza para recibir los datos necesarios para crear un nuevo producto
+ * incluyendo información de la categoría y el proveedor.
+ */
+
+public class ProductoCreationDTO {
+}
