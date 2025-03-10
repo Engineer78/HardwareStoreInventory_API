17cc214ee29a7f288531261ead88dfa63e45ec57
EliteBoku <devijunior3223@gmail.com>
2025-03-09 20:22:25 -0500

    Se realizan las importaciones necesarias para gestionar la información de la relación ProductoProveedor

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ProductoProveedorDTO.java
@@ -0,0 +1,9 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.dto;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * DTO para gestionar la información de ProductoProveedor.
+ */
+public class ProductoProveedorDTO {
+}
