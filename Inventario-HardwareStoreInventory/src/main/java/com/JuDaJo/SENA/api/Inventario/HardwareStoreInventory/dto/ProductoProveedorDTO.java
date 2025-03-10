1d3472bcca39bfb2f6d59c0b51d1e5db35b5e311
EliteBoku <devijunior3223@gmail.com>
2025-03-09 20:23:53 -0500

    Se añaden validaciones a los atributos de ProductoProveedorDTO

--- Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ProductoProveedorDTO.java
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ProductoProveedorDTO.java
@@ -6,4 +6,15 @@ import jakarta.validation.constraints.NotNull;
  * DTO para gestionar la información de ProductoProveedor.
  */
 public class ProductoProveedorDTO {
+    private int idProductoProveedor;
+
+    @NotNull(message = "El producto es obligatorio")
+    private int idProducto;
+
+    @NotNull(message = "El proveedor es obligatorio")
+    private int idProveedor;
+
+    @Min(value = 0, message = "El precio de compra debe ser mayor o igual a 0")
+    private double precioCompra;
+
 }
