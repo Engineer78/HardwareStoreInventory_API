bbe6ecf2625444fdcf3e5aa0d8a79196129b55fa
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-09 22:03:39 -0500

    Se crea la clase CategoriaController con su respectivo @RequestMapping(/api/categorias)

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/controller/CategoriaController.java
@@ -0,0 +1,17 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.controller;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.dto.CategoriaDTO;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Categoria;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.repository.CategoriaRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+}
