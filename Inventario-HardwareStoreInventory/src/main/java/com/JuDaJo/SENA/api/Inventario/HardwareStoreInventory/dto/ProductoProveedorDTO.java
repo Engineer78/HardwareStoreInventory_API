5d08b7d09177ea05c5e1be381b1a4fcd3868fa5b
EliteBoku <devijunior3223@gmail.com>
2025-03-09 20:25:33 -0500

    Se añade constructor vacío y constructor con parámetros en ProductoProveedorDTO

--- Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ProductoProveedorDTO.java
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ProductoProveedorDTO.java
@@ -16,5 +16,15 @@ public class ProductoProveedorDTO {
 
     @Min(value = 0, message = "El precio de compra debe ser mayor o igual a 0")
     private double precioCompra;
+    // Constructor vacío
+    public ProductoProveedorDTO() {}
+
+    // Constructor con parámetros
+    public ProductoProveedorDTO(int idProductoProveedor, int idProducto, int idProveedor, double precioCompra) {
+        this.idProductoProveedor = idProductoProveedor;
+        this.idProducto = idProducto;
+        this.idProveedor = idProveedor;
+        this.precioCompra = precioCompra;
+    }
 
 }
