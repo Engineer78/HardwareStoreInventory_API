48fffda4276113fdae9294fa33e4cbf629c78640
Juan Gallego <homertrixmemo@yahoo.com>
2025-03-09 20:12:10 -0500

    Se crea la clase ProveedorDTO y se hacen las importaciones

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ProveedorDTO.java
@@ -0,0 +1,10 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * DTO para gestionar la información de un proveedor.
+ */
+public class ProveedorDTO {
+}
