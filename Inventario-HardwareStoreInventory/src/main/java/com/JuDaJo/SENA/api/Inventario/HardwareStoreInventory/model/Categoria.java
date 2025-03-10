43cc5848396342f3ad58febfb143623739b77716
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-09 21:42:10 -0500

    Se crea la clase categoria con sus respectivas importaciones

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/model/Categoria.java
@@ -0,0 +1,14 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Entidad que representa una categoría de productos.
+ */
+@Entity
+public class Categoria {
+}
