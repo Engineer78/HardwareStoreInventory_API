b1784e240474f5bc581284242e8f0dbe0b12e827
Juan Gallego <homertrixmemo@yahoo.com>
2025-03-09 20:15:27 -0500

    Se definen los atributos en la clase ProveedorDTO

--- Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ProveedorDTO.java
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/ProveedorDTO.java
@@ -7,4 +7,20 @@ import jakarta.validation.constraints.Size;
  * DTO para gestionar la información de un proveedor.
  */
 public class ProveedorDTO {
+
+    private int idProveedor;
+
+    @NotBlank(message = "El nombre del proveedor es obligatorio")
+    @Size(max = 100, message = "El nombre no puede exceder los 100 caracteres")
+    private String nombreProveedor;
+
+    @NotBlank(message = "El NIT del proveedor es obligatorio")
+    @Size(max = 20, message = "El NIT no puede exceder los 20 caracteres")
+    private String nitProveedor;
+
+    @Size(max = 15, message = "El teléfono no puede superar los 15 caracteres")
+    private String telefonoProveedor;
+
+    @Size(max = 255, message = "La dirección no puede superar los 255 caracteres")
+    private String direccionProveedor;
 }
