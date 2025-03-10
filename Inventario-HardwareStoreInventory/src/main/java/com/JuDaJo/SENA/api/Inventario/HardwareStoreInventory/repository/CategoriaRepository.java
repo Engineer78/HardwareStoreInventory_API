76d38cf75edbf87f453ed1ea37b98d1ae2b12865
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-09 22:36:01 -0500

    Se crea la interfaz CategoriaRepository con sus importaciones respectivas

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.repository;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repositorio para la entidad Categoria.
+ * * Este repositorio proporciona operaciones CRUD básicas para la entidad Categoria
+ * utilizando Spring Data JPA.
+ */
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
