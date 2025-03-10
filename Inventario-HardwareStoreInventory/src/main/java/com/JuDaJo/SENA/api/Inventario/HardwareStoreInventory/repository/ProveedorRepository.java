32e431c50c5a94925ea3b17b0fced66a20acc965
Juan Gallego <homertrixmemo@yahoo.com>
2025-03-09 19:54:59 -0500

    Se crea el método buscar un proveedor por su NIT en la interface ProveedorRepository

--- Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/repository/ProveedorRepository.java
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/repository/ProveedorRepository.java
@@ -12,5 +12,16 @@ import java.util.Optional;
  * utilizando Spring Data JPA.
  */
 @Repository
-public interface ProveedorRepository {
+public interface ProveedorRepository extends JpaRepository<Proveedor, Integer> {
+    // No se necesitan métodos personalizados por ahora, ya que JpaRepository
+    // proporciona los métodos básicos (save, findById, findAll, delete, etc.).
+
+    /**
+     * Busca un proveedor por su NIT.
+     *
+     * @param nitProveedor El NIT del proveedor.
+     * @return Un Optional que contiene el proveedor encontrado, o un Optional vacío si no se encuentra.
+     */
+    Optional<Proveedor> findByNitProveedor(String nitProveedor);
+
 }
