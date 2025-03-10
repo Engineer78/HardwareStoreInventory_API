068eb8dfd462e9df7bc9c56c36684c9978e26c50
EliteBoku <devijunior3223@gmail.com>
2025-03-09 20:18:07 -0500

    Se implementa el repositorio ProductoProveedorRepository con @Repository

--- Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/repository/ProductoProveedorRepository.java
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/repository/ProductoProveedorRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface ProductoProveedorRepository extends JpaRepository<ProductoProveedor, Integer> {
     // Ajustando los nombres según Producto.idProducto
     Optional<ProductoProveedor>
