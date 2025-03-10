e61163415f1eb58f6a856aa008550f176cd511f0
EliteBoku <devijunior3223@gmail.com>
2025-03-09 20:09:56 -0500

    Se realizan las importaciones necesarias para ProductoProveedorRepository

---
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/repository/ProductoProveedorRepository.java
@@ -0,0 +1,10 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.repository;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.ProductoProveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface ProductoProveedorRepository {
+}
