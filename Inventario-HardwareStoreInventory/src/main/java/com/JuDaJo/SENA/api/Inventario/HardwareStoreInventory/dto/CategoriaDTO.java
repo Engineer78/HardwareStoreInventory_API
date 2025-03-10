0b6c318cc3004f24af77fb3f76cfb51ccbc46e9b
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-09 21:24:46 -0500

    Se crea la validación condicional si solo idCategoria es null y se ponen restricciones a la cantidad de caracteres del nombre de la categoría

--- Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/CategoriaDTO.java
+++ Inventario-HardwareStoreInventory/src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/dto/CategoriaDTO.java
@@ -6,5 +6,9 @@ import jakarta.validation.constraints.Size;
 public class CategoriaDTO {
 
     private Integer idCategoria;
-    
+
+    // Validación condicional: Solo requerido si idCategoria es nulo
+    @NotBlank(message = "El nombre de la categoría es obligatorio")
+    @Size(max = 100, message = "El nombre de la categoría no puede exceder los 100 caracteres")
+    private String nombreCategoria;
 }
